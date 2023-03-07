c54047d4da4df529bb449d3f84e480e3771eb669
nogadm <nogadm@gmail.com>
2023-03-07 17:16:08 +0200

    Update JsonUtils.java

--- src/main/java/utils/JsonUtils.java
+++ src/main/java/utils/JsonUtils.java
@@ -2,13 +2,16 @@ package utils;
 
 import com.google.gson.Gson;
 
+// A utility class for working with JSON strings using Google's Gson library
 public class JsonUtils {
     private static final Gson gson = new Gson();
 
+    // Parses the specified JSON string into an object of the specified type
     public static <T> T fromJson(String json, Class<T> classOfT) {
         return gson.fromJson(json, classOfT);
     }
 
+    // Serializes the specified object to a JSON string
     public static String toJson(Object object) {
         return gson.toJson(object);
     }
